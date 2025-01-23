beb327385441aa3189ac2f55e188289e143302bc
Shamary Williams <shamarywilliams@gmail.com>
2025-01-22 21:59:29 -0500

    reduce copyright font size

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -207,7 +207,7 @@ const Footer = () => {
         </div>
         <div className="bg-primary/10 py-8">
           <div className="container">
-            <p className="text-center text-base text-body-color dark:text-white">
+            <p className="text-center text-xs text-body-color dark:text-white">
               {/* Template by UIdeck and <a href="https://nextjstemplates.com" rel="nofollow noopener">Next.js Templates</a> */}
               © {new Date().getFullYear()} ADT
             </p>
