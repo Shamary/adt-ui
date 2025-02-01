0c02345f666723b23d677f4cc5781617c20bcde5
Shamary Williams <shamarywilliams@gmail.com>
2025-02-01 18:18:27 -0500

    add lucide package for drop down

--- components/About/AboutSectionTwo.tsx
+++ components/About/AboutSectionTwo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const AboutSectionTwo = () => {
   return (
     <section className="py-16 md:py-20 lg:py-28">
-      {/* <div className="container">
+      <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/2">
             <div
@@ -49,7 +49,7 @@ const AboutSectionTwo = () => {
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </section>
   );
 };
