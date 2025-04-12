7f65de802dd9cb493d04ecabb8f0aefc73ef1924
Shamary Williams <shamarywilliams@gmail.com>
2025-04-12 08:29:00 -0500

    optional callback parameter

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -2,14 +2,16 @@ import { redirect } from 'next/navigation';
 import CallbackHandler from './CallBackHandler';
 
 interface CallbackPageProps {
-    searchParams: {
-        code?: string;
-        error?: string;
-        [key: string]: string | string[] | undefined;
-    };
+  searchParams?: {
+    code?: string;
+    error?: string;
+    [key: string]: string | string[] | undefined;
+  };
 }
 
-export default async function CallbackPage({ searchParams }: CallbackPageProps) {
+export default async function CallbackPage({ 
+  searchParams = {}  // Provide default value
+}: CallbackPageProps = {}) {
     const code = searchParams.code;
     const error = searchParams.error;
 
