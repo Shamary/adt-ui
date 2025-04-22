e6ae2d8744131de0c2d0492c9ecc618573fcf516
Shamary Williams <shamarywilliams@gmail.com>
2025-04-21 20:51:45 -0500

    force dynamic callback page

--- next.config.js
+++ next.config.js
@@ -26,16 +26,6 @@ const nextConfig = {
           },
         ],
       },
-      // Add exception for auth callback
-      {
-        source: "/auth/callback",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "unsafe-none", // Less restrictive
-          },
-        ],
-      },
     ];
   },
 };
